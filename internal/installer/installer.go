4e08f9fe0dbcb795cba92ea61946e425da1dd75f
MohsenBg <moh.1380.1393@gmail.com>
2026-08-31 20:33:57 +0330

    installer: drop stray debug print

--- internal/installer/installer.go
+++ internal/installer/installer.go
@@ -364,7 +364,6 @@ func readChoice(input io.Reader) int {
 // isSupported restricts installs to platforms the release assets cover:
 // Linux, macOS, Android (Termux), and Windows.
 func isSupported(info platform.Info) bool {
-	fmt.Println(info.OS)
 	switch info.OS {
 	case platform.Linux, platform.MacOS, platform.Android, platform.Windows:
 	default:
